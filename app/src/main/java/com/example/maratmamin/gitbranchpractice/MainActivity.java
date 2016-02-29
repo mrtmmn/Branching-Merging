72e46f8f2f2864edce4832482fb53394021a29cf
mrtmmn <mrtmmn@gmail.com>
2016-02-29 12:51:11 -0500

    Practice Merge

--- app/src/main/java/com/example/maratmamin/gitbranchpractice/MainActivity.java
+++ app/src/main/java/com/example/maratmamin/gitbranchpractice/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.maratmamin.gitbranchpractice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Actor> mMoneyback = new ArrayList<Actor>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Actor actor = new Actor("Leo DiCap", "Aug 14", 4);
+        mMoneyback.add(actor);
+
+
+
     }
 }
